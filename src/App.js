bdda0664977bc5fe751f6fc8da92fabb7fe2531c
Jonathan Christopher Jakub <jonathanchristopher1199@gmail.com>
2020-11-28 13:58:12 +0700

    Display local query result

--- src/App.js
+++ src/App.js
@@ -15,34 +15,35 @@ const App = () => {
   const [localStore, setLocalStore] = useState(null);
   const [queryResult, setQueryResult] = useState([]);
   const [selectedResult, setSelectedResult] = useState({});
-  const [tempData, setTempData] = useState([]);
-
-  const getLocalStoreResult = useCallback((result) => {
-    console.log(result);
-    setTempData([...tempData, ...parseLocalToData(result)]);
-  }, []);
 
   useEffect(async () => {
     setIsLoading(true);
     const store = await createStore();
+    console.log(store);
     setLocalStore(store);
     setIsLoading(false);
   }, []);
 
   const onSearch = useCallback(async (value, type) => {
+    let results = [];
+  
     if (type === "0" || type === "2") {
       const dbPediaQuery = getCountryFromDbpedia(value);
       const dbPediaQueryResult = await queryToDbpedia(dbPediaQuery);
       const dbPediaResult = parseDbpediaToData(dbPediaQueryResult.results.bindings);
-      setTempData([...tempData, dbPediaResult]);
+      results = [...results, ...dbPediaResult];
     }
 
     if (type === "1" || type === "2") {
+      let localStoreQueryResult;
+      const getLocalStoreResult = (result) => { localStoreQueryResult = result;};
       const localStoreQuery = getCountryFromLocalStore();
       await queryToLocalStore(localStore, localStoreQuery, getLocalStoreResult);
+      const localStoreResult = parseLocalToData(localStoreQueryResult);
+      results = [...results, ...localStoreResult];
     }
 
-    setQueryResult(tempData);
+    setQueryResult(results);
     setSelectedResult({});
   }, []);
 
