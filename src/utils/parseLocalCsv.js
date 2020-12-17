cec97c0c4fee8d2ce5a286d6326d07f9e90ae807
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-12-17 14:05:55 +0700

    Remove unused columns/attributes from local RDF store.

--- src/utils/parseLocalCsv.js
+++ src/utils/parseLocalCsv.js
@@ -35,17 +35,7 @@ export const addDataToStore = (store, df) => {
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "inRegion", row.get("Region"), "string");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "population", row.get("Population"), "long");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "areaInSquareMile", row.get("Area (sq. mi.)"), "long");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "populationDensityPerSquareMile", row.get("Pop. Density (per sq. mi.)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "coastlineRatio", row.get("Coastline (coast/area ratio)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "netMigration", row.get("Net migration"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "infantMortalityPer1000", row.get("Infant mortality (per 1000 births)"), "double");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "gdpPerCapita", row.get("GDP ($ per capita)"), "long");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "literacyPercent", row.get("Literacy (%)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "phonesPer1000", row.get("Phones (per 1000)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "arablePercent", row.get("Arable (%)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "cropsPercent", row.get("Crops (%)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "otherPercent", row.get("Other (%)"), "double");
-    query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "climate", row.get("Climate"), "double");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "birthrate", row.get("Birthrate"), "double");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "deathrate", row.get("Deathrate"), "double");
     query += addLiteralTriple(row.get("Country").replace(/\s/g,"_").replace(/[^_a-zA-Z0-9]/g,"-").toLowerCase(), "agriculture", row.get("Agriculture"), "double");
@@ -80,7 +70,7 @@ async function createStore(localStore) {
       console.log("[rdfstore.create] Creating store success");
       store.registerDefaultProfileNamespaces();
       store.registerDefaultNamespace("sam", "http://www.samkok.cn/resource/");
-  
+
       const df = await parseLocalCSV();
       addDataToStore(store, df);
       localStore.obj = store;
